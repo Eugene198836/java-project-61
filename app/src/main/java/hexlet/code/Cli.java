1647a704abda7f3253dd00d825189e7867e2ec99
An Eugene <gevtonik@gmail.com>
2024-04-10 17:48:33 +0900

    step 4

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,13 +2,11 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-	public static void scan() {
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("May I have your name? ");
-        	String userName = scanner.next();
-        	System.out.println("Hello, " + userName + "!");
-
-        	scanner.close();
-		}
+    public static void scan() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+        scanner.close();
+    }
 }
