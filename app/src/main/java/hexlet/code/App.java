144cc21b4f42bf353c974d7801b807c9a803b26c
An Eugene <gevtonik@gmail.com>
2024-04-16 11:59:24 +0900

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,6 +6,7 @@ public class App {
     public static void main(String[] args) {
 
 // Вывод на экран меню выбора игр.
+        System.out.println("Welcome to the Brain Games!");
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet\n" + "2 - Even\n" +  "3 - Calc\n" + "4 - GCD\n" + "5 - Progression\n"
                 + "6 - Prime\n" + "0 - Exit");
@@ -14,7 +15,17 @@ public class App {
         Scanner choose = new Scanner(System.in);
         String yourDestiny = choose.next();
         System.out.println("Your choice: " + yourDestiny);
-        int number = Integer.parseInt(yourDestiny);
+        int number = 0;
+
+// Перехват ошибки, если будут введены не числа, а буквы, спец.знаки и др.
+        try {
+            number = Integer.parseInt(yourDestiny);
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter the game number and press Enter.");
+            System.out.println("Let's try again!");
+
+        }
+
         switch (number) {
             case 1:
                 Greet.mayIhaveYourName();
@@ -35,7 +46,6 @@ public class App {
                 Prime.primeNumber();
                 break;
             case 0:
-                System.out.println("Good bye");
                 break;
         }
 
